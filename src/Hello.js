e180a7a295fe78ff6cc01c8940a6cbd8720356ee
Hassannawazish <50699910+Hassannawazish@users.noreply.github.com>
2020-07-24 07:57:31 +0500

    Myfirstapplication

---
+++ src/Hello.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Hello.css'
+function Hello({firstname}) {
+  return <p className="myname" ><strong>Electrical Engineer {firstname}</strong></p>
+} 
+
+export default Hello;
